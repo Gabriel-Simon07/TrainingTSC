cc7ed50f37dd247b7dbcdb26b7bdb67c79722251
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-02 22:32:46 -0300

    Learning how use function map() in typescript

--- part1.js
+++ part1.js
@@ -1,4 +1,15 @@
 "use strict";
 exports.__esModule = true;
-var x = 'Hello World 2';
-console.log(x);
+var list1 = [1, 2, 3, 4];
+var list2 = [];
+var nomes = ['MARIA', 'JOAO', 'ANABELA'];
+// --------------------------------------------------------------------------------------------------------------------
+// map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção com os elementos alterados
+function double(x) {
+    return x * 2;
+}
+function triple(x) {
+    return x * 3;
+}
+var m1 = list1.map(double);
+console.log(m1);
--- part1.ts
+++ part1.ts
@@ -1,4 +1,20 @@
 export { }
 
-let x = 'Hello World 2';
-console.log(x);
+let list1 = [1, 2, 3, 4];
+let list2 = [];
+let nomes = ['MARIA', 'JOAO', 'ANABELA'];
+
+// --------------------------------------------------------------------------------------------------------------------
+// map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção com os elementos alterados
+
+function double(x: number):number {
+    return x * 2;
+}
+
+function triple(x: number):number {
+    return x * 3;
+}
+
+let m1 = list1.map(double);
+
+console.log(m1);
