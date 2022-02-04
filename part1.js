52f91c07dbd86e15852ed8b675a19db98241f78a
Gabriel Simon <gabrielsimon775@gmail.com>
2022-02-03 21:38:57 -0300

    Implemented functions using filter and map

--- part1.js
+++ part1.js
@@ -2,7 +2,8 @@
 exports.__esModule = true;
 var list1 = [1, 2, 3, 4];
 var list2 = [];
-var nomes = ['MARIA', 'JOAO', 'ANABELA'];
+var nomes = ["MARIA", "JOAO", "ANABELA"];
+var listLetters = ["G", "A", "B", "J"];
 // --------------------------------------------------------------------------------------------------------------------
 // map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção com os elementos alterados
 function double(x) {
@@ -12,4 +13,39 @@ function triple(x) {
     return x * 3;
 }
 var m1 = list1.map(double);
+var m2 = list1.map(triple);
+var m3 = list1.map(function (x) { return x * 4; });
+console.log("MAP Results ------------------------------");
 console.log(m1);
+console.log(m2);
+console.log(m3);
+// --------------------------------------------------------------------------------------------------------------------
+// filter: retorna uma nova coleção contendo apenas aqueles elementos da coleção original que satisfazem um dado predicado
+function pair(x) {
+    return x % 2 == 0;
+}
+function startWithA(letter) {
+    return letter == "A";
+}
+console.log("FILTER Results ------------------------------");
+console.log(list1.map(pair));
+console.log(listLetters.map(startWithA));
+console.log(listLetters.filter(function (letter) { return letter == "A"; }));
+console.log(list1.map(function (pair) { return pair % 2 == 0; }));
+console.log(list1.filter(function (number) { return number % 2 != 0; }));
+console.log(list1.filter(function (number) { return number > 2; }));
+// --------------------------------------------------------------------------------------------------------------------
+// reduce: aplica cumulativamente uma função aos elementos de uma coleção, retornando o resultado final.
+// * você pode informar, opcionalmente, um valor inicial como parâmetro (necessário para coleção vazia).
+function sum(x, y) {
+    return x + y;
+}
+function multiply(x, y) {
+    return x * y;
+}
+console.log("REDUCE Results ------------------------------");
+console.log(list1.reduce(sum));
+console.log(list2.reduce(sum, 1));
+console.log(list1.reduce(multiply));
+// --------------------------------------------------------------------------------------------------------------------
+// sort: ordena a coleção conforme a função de comparação informada como parâmetro
--- part1.ts
+++ part1.ts
@@ -1,20 +1,73 @@
-export { }
+export {};
 
 let list1 = [1, 2, 3, 4];
 let list2 = [];
-let nomes = ['MARIA', 'JOAO', 'ANABELA'];
+let nomes = ["MARIA", "JOAO", "ANABELA"];
+let listLetters: string[] = ["G", "A", "B", "J"];
 
 // --------------------------------------------------------------------------------------------------------------------
 // map: aplica uma função a cada elemento da coleção original, retornando uma nova coleção com os elementos alterados
 
-function double(x: number):number {
-    return x * 2;
+function double(x: number): number {
+  return x * 2;
 }
 
-function triple(x: number):number {
-    return x * 3;
+function triple(x: number): number {
+  return x * 3;
 }
 
 let m1 = list1.map(double);
+let m2 = list1.map(triple);
+let m3 = list1.map((x) => x * 4);
 
+console.log("MAP Results ------------------------------");
 console.log(m1);
+console.log(m2);
+console.log(m3);
+
+// --------------------------------------------------------------------------------------------------------------------
+// filter: retorna uma nova coleção contendo apenas aqueles elementos da coleção original que satisfazem um dado predicado
+
+function pair(x: number): boolean {
+  return x % 2 == 0;
+}
+
+function startWithA(letter: string): boolean {
+  return letter == "A";
+}
+
+console.log("FILTER Results ------------------------------");
+console.log(list1.map(pair));
+console.log(listLetters.map(startWithA));
+console.log(listLetters.filter((letter) => letter == "A"));
+console.log(list1.map((pair) => pair % 2 == 0));
+console.log(list1.filter((number) => number % 2 != 0));
+console.log(list1.filter((number) => number > 2));
+
+// --------------------------------------------------------------------------------------------------------------------
+// reduce: aplica cumulativamente uma função aos elementos de uma coleção, retornando o resultado final.
+// * você pode informar, opcionalmente, um valor inicial como parâmetro (necessário para coleção vazia).
+
+function sum(x: number, y: number): number {
+    return x + y;
+}
+
+function multiply(x: number, y: number): number {
+    return x * y;
+}
+
+console.log("REDUCE Results ------------------------------");
+
+console.log(list1.reduce(sum));
+
+console.log(list2.reduce(sum, 1));
+
+console.log(list1.reduce(multiply));
+
+// --------------------------------------------------------------------------------------------------------------------
+// sort: ordena a coleção conforme a função de comparação informada como parâmetro
+
+
+
+
+
